c5674f7e94f090deef28b1666aabcd9f0e2c6989
geraldineDafa <dafageraldine16@gmail.com>
2023-01-28 05:36:23 +0700

    add smtp

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,flash,redirect,url_for
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -6,9 +6,10 @@ from flask_cors import CORS
 from datetime import datetime
 # from redis import Redis
 from redislite import Redis
-import json
+import json, smtplib
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 CORS(app)
 redis_server = Redis('/tmp/redis.db')
 # redis_server = Redis()
@@ -25,9 +26,51 @@ tblproduct = dbq.collection('investment product')
 tblrecord = dbq.collection('investment record')
 tbltype = dbq.collection('investment type')
 tbluser = dbq.collection('loginuser')
+s = smtplib.SMTP('smtp.gmail.com',587)
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def index():
+    if request.method == 'POST':
+        mails = request.form['email']
+        from_ = request.form['name']
+        msg = request.form['message']
+        fromredis = redis_server.get(mails)
+        if(fromredis):
+            flash("mail server busy, try again later or whatsapp me instead !","warn")
+            return redirect(url_for('index')+'#contact')
+        else:
+            if mails != '' and from_  != '' and msg != '':
+                sender = "mybotmailer2023@gmail.com"
+                rec = ["dafageraldine16@gmail.com",mails]
+                pwd = "movsydghkpnxqiad"
+                msg_embed = """From: <%s>
+    To: <%s>
+    Subject: Sended From Portfolio Web
+
+    <%s>
+    Pesan ini dikirim melalui smtplib dan diterima oleh modul SMTP Server Python.
+
+    """ %(from_,str(rec[0])+','+str(rec[1]),'Dari '+ str(from_) + ' untuk dafa geraldine, ' + str(msg))
+                try:
+                    s.starttls()
+                    s.login(sender,pwd)
+                    s.sendmail(sender,rec,msg_embed)
+                    redis_server.set(mails,mails)
+                    redis_server.expire(mails,7200)
+                    flash("an email has been sent, check your mail box !","success")
+                    return redirect(url_for('index')+'#contact')
+                except:
+                    try:
+                        s.login(sender,pwd)
+                        s.sendmail(sender,rec,msg_embed)
+                        redis_server.set(mails,mails)
+                        redis_server.expire(mails,7200)
+                        flash("an email has been sent, check your mail box !","success")
+                        return redirect(url_for('index')+'#contact')
+                    except:
+                        flash("error send email, please try again later or whatsapp instead !","danger")
+                        return redirect(url_for('index')+'#contact')
+
     return render_template("index.html")
 
 @app.route('/pomimonitoring')
